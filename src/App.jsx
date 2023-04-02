a3a6bdde09e98cbf768fb8bf965fb73a81ab9a39
Nishant Mehta <nishme0708@gmail.com>
2023-04-02 23:04:13 +0530

    Add code changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,35 @@
+import { useState } from 'react';
+import Title from './Title';
+import menu from './data';
+import Menu from './Menu';
+import Category from './Category';
+
+const ALL = 'all';
+const allCategories = [ALL, ...new Set(menu.map((item) => item.category))];
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  const [menuItems, setMenuItems] = useState(menu);
+  const [category, setCategory] = useState(ALL);
+  const handleCategory = (category) => {
+    setCategory(category);
+    if (category == ALL) {
+      setMenuItems(menu);
+    } else {
+      setMenuItems(menu.filter((menuItem) => menuItem.category == category));
+    }
+  };
+  console.log(menuItems);
+  return (
+    <main>
+      <section className='menu'>
+        <Title text={'Our Menu'}></Title>
+        <Category
+          categories={allCategories}
+          updateCategory={handleCategory}
+        ></Category>
+        <Menu items={menuItems}></Menu>
+      </section>
+    </main>
+  );
 };
 export default App;
